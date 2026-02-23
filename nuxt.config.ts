fc79af7a7e941a7d6251d63ff7cf0cbebd4e612e
Liang Mi <github@liangmi.dev>
2026-02-23 09:56:52 +0800

    chore: redesign index page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,28 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxt/eslint', '@unocss/nuxt'],
   devtools: { enabled: true },
-  
+
   eslint: {
     config: {
       standalone: false,
     },
   },
 
-  typescript:{
+  typescript: {
     typeCheck: true,
   },
-  
+
+  app: {
+    head: {
+      title: 'Liang',
+      meta: [
+        { name: 'description', content: 'Liang\'s Growth Timeline' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/avatar.png' },
+      ],
+    },
+  },
+
   compatibilityDate: '2024-04-03',
 })
