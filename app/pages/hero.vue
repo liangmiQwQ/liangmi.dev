3d29af91d6ba373961a5fe7f96d3e9a295ddcb2a
Liang Mi <github@liangmi.dev>
2026-04-19 14:34:10 +0800

    feat: adjust hero page

--- app/pages/hero.vue
+++ app/pages/hero.vue
@@ -1,9 +1,21 @@
 <script lang="ts" setup>
+
 </script>
 
 <template>
-  <div class="relative w-full h-screen overflow-hidden">
+  <div
+    flex="~ items-center justify-between col"
+    relative w-full h-screen overflow-hidden
+  >
     <HeroSky op80 />
     <HeroFirstMagnitudeStar />
+    <div text-2xl flex="~ col items-center">
+      <span>
+        Do Things I love
+      </span>
+      <span>
+        Learn about myself
+      </span>
+    </div>
   </div>
 </template>
