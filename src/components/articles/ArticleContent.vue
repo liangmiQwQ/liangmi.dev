4fbae70e88441301909fb3fdedba358805f02d95
Liang <github@liangmi.dev>
2026-08-21 21:15:03 +0800

    style: refine article layout

--- src/components/articles/ArticleContent.vue
+++ src/components/articles/ArticleContent.vue
@@ -5,5 +5,21 @@ defineProps<{
 </script>
 
 <template>
-  <article class="void-md" v-html="html" />
+  <article class="article-content void-md" v-html="html" />
 </template>
+
+<style scoped>
+.article-content {
+  --vmd-font-body: "Inter", system-ui, sans-serif;
+  --vmd-line-height: 1.8;
+
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-rendering: optimizeLegibility;
+}
+
+.article-content :deep(h2) {
+  padding-bottom: 0;
+  border-bottom: 0;
+}
+</style>
